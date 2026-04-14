acf0b01f2ce0a1960a037672d478d4006f122e0d
Aniket Mule <muleaniket012@email.com>
2026-04-14 12:55:57 +0530

    initial commit

---
+++ portfolio.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('.multi-text', {
+    strings: ['FrontEnd Developer', 'Excel Sheet Editor', 'Web Developer'],
+    typeSpeed: 50,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+  });
